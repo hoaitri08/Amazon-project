931b7c0e88078ae0c2c1a7c597a4250abedee0d6
hoaitri08 <tringuyen2004a@gmail.com>
2025-09-06 01:00:06 +0700

    fix 2

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,23 @@
-export const cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
   {
     productId: '1',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '1'
   },
   {
     productId: '2',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '2'
   }
 ];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 
 export function addToCart(productId) {
@@ -24,8 +34,39 @@ export function addToCart(productId) {
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
       });
     }
+
+    saveToStorage();
 }
 
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    };
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
